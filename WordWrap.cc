b91c5fbcc830b10610c7bdcf83b894c1a0a5199b
Yifan Jonathan Dai <yfjd01@gmail.com>
2014-06-03 00:36:10 -0400

    WordWrap
    Command line argument execution requires
    
    ./a.out File LineLength
    
    where File is a string that is on the same line and LineLength is a
    number (representing the length of the line)

---
+++ WordWrap.cc
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+ string theArg , theArgArg, theTest;
+
+
+
+    int wraplen = 25, wordlen = 0, currlen= 0;
+    //wraplen, length of ever lines wrap
+    //wordlen, length of the current word
+    //currlen, number of characters on current line
+
+    theArg = argv[1];
+
+    if(argc == 3){
+
+        theArgArg = argv[2];
+        fstream inarg;
+        inarg.open(theArgArg.c_str());
+        inarg >> wraplen;
+
+    }
+
+    ifstream infile;
+    infile.open(theArg.c_str()); //This is a c-style string, For some reason it doesnt accept normal strings
+    string word;
+
+
+
+    while (infile >> word)
+    {
+
+    wordlen = word.length();
+        //cout << wordlen;
+        if(currlen > 0 && currlen + wordlen <= wraplen){
+            cout<<' ';
+            currlen++;
+        }
+
+
+        if (wordlen > wraplen){
+
+            if(currlen > 0){
+                cout<< endl;
+            }
+            currlen = 0;
+
+            for(int i = 0; i < wraplen; i++){
+                cout << word.at(i);
+            }
+            cout << endl;
+
+            for(int i = wraplen; i < wordlen; i++){
+                cout << word.at(i);
+                currlen++;
+            }
+
+
+        }else if(wordlen + currlen > wraplen){
+
+            currlen = 0;
+            currlen += wordlen ;
+            cout << endl;
+            cout << word;
+
+            //if(currlen > wraplen){
+            //}
+
+
+        }else if(wordlen + currlen <= wraplen){
+            currlen += wordlen;
+            cout << word ;
+        }
+
+
+    }
+
+    cout << endl;
+
+}
